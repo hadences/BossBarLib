cf3cbe9e08e40e7494a3e92f6e837ecfdf0392d9
jaesu <60861660+hadences@users.noreply.github.com>
2024-10-01 01:02:26 -0500

    Gradle Update

--- src/main/java/net/hadences/BossBarLib.java
+++ src/main/java/net/hadences/BossBarLib.java
@@ -15,6 +15,7 @@ public class BossBarLib implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("bossbarlib");
 	public static final String MOD_ID = "bossbarlib";
 
+
 	@Override
 	public void onInitialize() {
 		LOGGER.info("BossBarLib initialized");
